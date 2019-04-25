84d8c8b7657d3f8eac80c13dacacff89044b53ab
Adam LaCasse <adamlacasse@outlook.com>
2019-04-25 16:29:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,26 @@
 function basicTeenager(age) {
-
+  if (age > 12 && age < 20) {
+    return "You are a teenager!";
+  }
 }
 
 function teenager(age) {
-
+  if (age > 12 && age < 20) {
+    return "You are a teenager!";
+  }
+  return "You are not a teenager";
 }
 
 function ageChecker(age) {
-
+  if (age > 12 && age < 20) {
+    return "You are a teenager!";
+  } else if (age < 13) {
+      return "You are a kid";
+    } return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
-
+  age > 12 && age < 20 ? return "You are a teenager" : return "You are not a teenager";
 }
 
 function switchAge(age) {
